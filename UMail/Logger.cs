e981b58ec379e28b2efdfaaa0920efb87de57f1f
Hobafizo <mohabfawzy2@gmail.com>
2023-08-01 21:49:34 +0300

    Add project files.

---
+++ UMail/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMail
+{
+    internal static class Logger
+    {
+        private static object LogLocker;
+
+        public static void Write(string message, params object[] args)
+        {
+
+        }
+    }
+}
